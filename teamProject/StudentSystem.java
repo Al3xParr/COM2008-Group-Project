05a046778ec3518d956db7d95784b8c7e73e5cf7
Zbigniew Lisak <zbigniew.lisak68@gmail.com>
2020-11-09 19:47:10 +0000

    Added some file structure

---
+++ teamProject/StudentSystem.java
@@ -0,0 +1,18 @@
+package teamProject;
+/**
+ * Team Project COM2008 year 20/21
+ * @author Nathan Mitchell
+ * @author Alex Parr
+ * @author Julia Jablonska
+ * @author Zbigniew Lisak 
+ */
+import teamProject.*;
+
+/** 
+ * Main class and starting point of the application
+*/
+public class StudentSystem {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ teamProject/database/Database.java
@@ -0,0 +1,19 @@
+package teamProject.database;
+/**
+ * Team Project COM2008 year 20/21
+ * @author Nathan Mitchell
+ * @author Alex Parr
+ * @author Julia Jablonska
+ * @author Zbigniew Lisak 
+ */
+
+
+ 
+/**
+ * Class representing and operating database
+ */
+public class Database {
+
+
+    
+}
