29310ac9174051d96cd7d0dd2a932a7973b3dd54
Zbigniew Lisak <zbigniew.lisak68@gmail.com>
2020-11-10 19:24:07 +0000

    Added some baseline code

--- teamProject/src/teamProject/StudentSystem.java
+++ teamProject/src/teamProject/StudentSystem.java
@@ -1,4 +1,9 @@
 package teamProject;
+
+import java.util.*;
+import java.sql.*;
+import teamProject.db.*;
+
 /**
  * Team Project COM2008 year 20/21
  * @author Nathan Mitchell
@@ -6,14 +11,25 @@ package teamProject;
  * @author Julia Jablonska
  * @author Zbigniew Lisak 
  */
-import teamProject.db.*;
 
 /** 
  * Main class and starting point of the application
 */
 public class StudentSystem {
+
+    final static String url = "//stusql.dcs.shef.ac.uk/team044";
+    final static String user = "team044";
+    final static String pass = "872345c0";
+
     public static void main(String[] args) {
-        Database x = new Database();
-        x.print();
+
+        try (Database db = new Database(url, user, pass)) {
+
+            //DO STUFF HERE
+            db.resetDB();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- teamProject/src/teamProject/db/Database.java
+++ teamProject/src/teamProject/db/Database.java
@@ -1,4 +1,5 @@
 package teamProject.db;
+
 /**
  * Team Project COM2008 year 20/21
  * @author Nathan Mitchell
@@ -7,15 +8,34 @@ package teamProject.db;
  * @author Zbigniew Lisak 
  */
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
- 
 /**
  * Class representing and operating database
  */
-public class Database {
+public class Database implements AutoCloseable {
 
-    public void print() {
-        System.out.println("Hello");
+    Connection con = null;
+
+    public Database(String url, String user, String password) throws SQLException {
+        String str = "jdbc:mysql:" + url + "?user=" + user + "&password=" + password;
+        con = DriverManager.getConnection(str);
     }
-    
+
+    public void resetDB() {
+        //reset and setup DB
+        //dummy code
+        System.out.println("Hey code works!");
+
+    }
+
+    //QUERIES AND DATA PROCESSING 
+
+    @Override
+    public void close() throws Exception {
+        con.close();
+    }
+
 }
