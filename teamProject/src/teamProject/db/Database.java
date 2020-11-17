ad3796c0130ff9828214284e19697aaff3f489a6
Zbigniew Lisak <zbigniew.lisak68@gmail.com>
2020-11-17 00:09:16 +0000

    Added some Primary code validation and a bit of formating

--- teamProject/src/teamProject/db/Database.java
+++ teamProject/src/teamProject/db/Database.java
@@ -33,12 +33,12 @@ public class Database implements AutoCloseable {
 
     }
 
-    //QUERIES AND DATA PROCESSING 
-
     @Override
     public void close() throws Exception {
         con.close();
     }
+
+    //QUERIES AND PROCESSING
 
     private Set<String> getTableNames() {
         Set<String> tables = null;
@@ -50,6 +50,7 @@ public class Database implements AutoCloseable {
             while (results.next()) {
                 tables.add(results.getString("TABLE_NAME"));
             }
+            results.close();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -69,6 +70,7 @@ public class Database implements AutoCloseable {
                 while (results.next()) {
                     columns.add(results.getString("COLUMN_NAME"));
                 }
+                results.close();
 
             } catch (Exception e) {
                 e.printStackTrace();
@@ -79,59 +81,53 @@ public class Database implements AutoCloseable {
     }
 
     /**
-     * Checks if given value is already used in a table
+     * Checks if given set of values exists within the same row of a table
      * @param tableName table to check
-     * @param columnName column to check
-     * @param value value to took for
-     * @return true if value exists in given coulmn or there was an error false otherwise
+     * @param columnNames Array of column names to check
+     * @param columnValues Array of values of primary key associated with column names
+     * @return true if given set of values exists in table false otherwise
      */
-    public boolean valueExists(String tableName, String columnName, String value) {
-        if (getTableNames().contains(tableName) && getColumnNames(tableName).contains(columnName)) {
-            try (Statement stsm = con.createStatement()) {
+    public boolean ValueSetCheck(String tableName, String[] columnNames, String[] columnValues) {
+        boolean succes = false;
+        if (columnNames.length == columnValues.length && columnNames.length != 0) {
+            for (String name : columnNames)
+                if (!getColumnNames(tableName).contains(name))
+                    return false;
 
-                ResultSet results = stsm.executeQuery("SELECT DISTINCT " + columnName + " FROM " + tableName + ";");
-
-                while (results.next()) {
-                    if (results.getString(1) == value) {
-                        return true;
-                    }
+            String query = "SELECT * FROM " + tableName + "WHERE";
+            int i = 0;
+            query += " " + columnNames[i++] + " = ?";
+            for (; i < columnNames.length; i++) {
+                query += " AND " + columnNames[i] + " = ?";
+            }
+            query += ";";
+            try (PreparedStatement check = con.prepareStatement(query)) {
+                check.clearParameters();
+                int index = 1;
+                for (String s : columnValues) {
+                    check.setString(index++, s);
                 }
-                return false;
-
+                ResultSet res = check.executeQuery();
+                if (res.next()) {
+                    res.close();
+                    succes = true;
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        return true;
+        return succes;
+
     }
 
-    //INSERT FUNCTIONS
-
-    private void insertStudyPeriod(StudyPeriod newPeriod, Student student) throws SQLException {
-        String insertPeriod = "INSERT INTO StudentsToModules VALUES(?,?,?,?,?,?,?);";
-        try (PreparedStatement insert = con.prepareStatement(insertPeriod)) {
-            for (Grade g : newPeriod.getGradesList()) {
-                insert.clearParameters();
-                insert.setInt(1, student.getRegNum());
-                insert.setString(2, g.getModule().getModuleCode());
-                insert.setDouble(3, g.getMark());
-                insert.setDouble(4, g.getResitMark());
-                insert.setString(5, "" + newPeriod.getLabel());
-                insert.setString(6, student.getCourse().getCourseCode());
-                insert.setString(7, "" + newPeriod.getDegreeLvl().getDegreeLvl());
-                insert.executeUpdate();
-            }
-        } catch (SQLException e) {
-            throw e;
-        }
-    }
+    //INSERTS - PUBLIC FUNCTIONS
 
     /**
-     * Inserts information about new study period into database
-     * @param newPeriod new StudyPeriod object to be assigned to student
-     * @param to Student the study period belongs to
-     * @return true if operation was compleated false if not
-     */
+    * Inserts information about new study period into database
+    * @param newPeriod new StudyPeriod object to be assigned to student
+    * @param to Student the study period belongs to
+    * @return true if operation was compleated false if not
+    */
     public boolean addStudyPeriod(StudyPeriod newPeriod, Student to) {
         boolean succes = true;
         try {
@@ -145,83 +141,70 @@ public class Database implements AutoCloseable {
 
     }
 
-    private void insertStudent(Student newStudent) throws SQLException {
-
-        String insertStudent = "INSERT INTO Students VALUES(?,?,?,?,?,?,?,?)";
-        try (PreparedStatement insert = con.prepareStatement(insertStudent)) {
-            insert.clearParameters();
-            insert.setInt(1, newStudent.getRegNum());
-            insert.setString(2, newStudent.getTitle());
-            insert.setString(3, newStudent.getSurname());
-            insert.setString(4, newStudent.getForenames());
-            insert.setString(5, newStudent.getEmail());
-            insert.setString(6, newStudent.getUsername());
-            insert.setString(7, newStudent.getTutor());
-            insert.setString(8, newStudent.getCourse().getCourseCode());
-            insert.executeUpdate();
-            for (StudyPeriod x : newStudent.getStudyPeriodList()) {
-                insertStudyPeriod(x, newStudent);
-            }
-        } catch (SQLException e) {
-            throw e;
-        }
-    }
-
-    private void insertTeacher(Teacher newTeacher) throws SQLException {
-
-        String insertTeacher = "INSERT INTO Teachers VALUES(?,?)";
-        try (PreparedStatement insert = con.prepareStatement(insertTeacher)) {
-            insert.clearParameters();
-            insert.setString(1, newTeacher.getUsername());
-            insert.setString(2, newTeacher.getFullName());
-            insert.executeUpdate();
-
-        } catch (SQLException e) {
-            throw e;
-        }
-    }
-
     /**
      * Inserting newly created user into database (inserts all relative information)
      * @param newUser user to be inserted
      * @return true if user inserted succesfully false if not
      */
     public boolean addUser(User newUser) {
-
-        String type = "Basic";
-        if (newUser instanceof Student) {
-            type = "Student";
-        } else if (newUser instanceof Teacher) {
-            type = "Teacher";
-        } else if (newUser instanceof Administrator) {
-            type = "Administrator";
-        } else if (newUser instanceof Registrar) {
-            type = "Registrar";
-        }
+        
         boolean succes = true;
-        String insertUser = "INSERT INTO Account VALUES(?,?,?,?);";
-        try (PreparedStatement insert = con.prepareStatement(insertUser)) {
-            insert.clearParameters();
-            insert.setString(1, newUser.getUsername());
-            insert.setString(2, newUser.getPasswordHash());
-            insert.setString(3, newUser.getSalt());
-            insert.setString(4, type);
-            insert.executeUpdate();
-            switch (type) {
-                case "Student":
-                    insertStudent((Student) newUser);
-                    break;
-                case "Teacher":
-                    insertTeacher((Teacher) newUser);
-                    break;
+        String[] names = { "username" };
+        String[] values = { newUser.getUsername() };
+        
+        if (!ValueSetCheck("Accounts", names, values)) {
+            String type = "Basic";
+            if (newUser instanceof Student) {
+                type = "Student";
+            } else if (newUser instanceof Teacher) {
+                type = "Teacher";
+            } else if (newUser instanceof Administrator) {
+                type = "Administrator";
+            } else if (newUser instanceof Registrar) {
+                type = "Registrar";
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-            succes = false;
+
+            String insertUser = "INSERT INTO Account VALUES(?,?,?,?);";
+            try (PreparedStatement insert = con.prepareStatement(insertUser)) {
+                insert.clearParameters();
+                insert.setString(1, newUser.getUsername());
+                insert.setString(2, newUser.getPasswordHash());
+                insert.setString(3, newUser.getSalt());
+                insert.setString(4, type);
+                insert.executeUpdate();
+                switch (type) {
+                    case "Student":
+                        insertStudent((Student) newUser);
+                        break;
+                    case "Teacher":
+                        insertTeacher((Teacher) newUser);
+                        break;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                succes = false;
+            }
         }
 
         return succes;
 
+    }
+
+    /**
+     * Inserts information about a course into database
+     * @param newCourse
+     * @return true if operation was succesfull
+     */
+    public boolean addCourse(Course newCourse) {
+        boolean succes = false;
+        try {
+            insertCourse(newCourse);
+            succes = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return succes;
     }
 
     /**
@@ -230,41 +213,34 @@ public class Database implements AutoCloseable {
      * @return true if the opperation was succesful
      */
     public boolean addDepartment(Department newDepartment) {
-        boolean succes = true;
+        boolean succes = false;
+        String[] names = { "deptCode" };
+        String[] values = { newDepartment.getDeptCode() };
+        if (!ValueSetCheck("Departments", names, values)) {
 
-        String insertDep = "INSERT INTO Departments VALUES (?,?);";
-        try (PreparedStatement insert = con.prepareStatement(insertDep)) {
-            insert.clearParameters();
-            insert.setString(1, newDepartment.getDeptCode());
-            insert.setString(2, newDepartment.getFullName());
-            insert.executeUpdate();
-            for (Course c : newDepartment.getCourseList()) {
-                insertCourseDepartLink(c, newDepartment);
-            }
-            for (Module m : newDepartment.getModuleList()) {
-                updateModuleSupervision(m, newDepartment);
-            }
+            String insertDep = "INSERT INTO Departments VALUES (?,?);";
+            
+            try (PreparedStatement insert = con.prepareStatement(insertDep)) {
+                insert.clearParameters();
+                insert.setString(1, newDepartment.getDeptCode());
+                insert.setString(2, newDepartment.getFullName());
+                insert.executeUpdate();
+                for (Course c : newDepartment.getCourseList()) {
+                    insertCourse(c);
+                    insertCourseDepartLink(c, newDepartment);
+                }
+                for (Module m : newDepartment.getModuleList()) {
+                    updateModuleSupervision(m, newDepartment);
+                }
+                succes = true;
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            succes = false;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
         return succes;
 
-    }
-
-    private void insertCourseDepartLink(Course c, Department d) throws SQLException{
-        String insertLink = "INSERT INTO CourseToDepartment VALUES(?,?,?);";
-        try (PreparedStatement insert = con.prepareStatement(insertLink)) {
-            insert.clearParameters();
-            insert.setString(1, c.getCourseCode());
-            insert.setString(2, d.getDeptCode());
-            insert.setBoolean(3, c.getMainDep() == d);
-            insert.executeUpdate();
-        } catch (SQLException e) {
-            throw e;
-        }
     }
 
     /**
@@ -284,22 +260,142 @@ public class Database implements AutoCloseable {
         return succes;
     }
 
-    //UPDATE QUERIES
+    //INSERTS - PRIVATE FUNCTIONS
 
-    private void updateModuleSupervision(Module m, Department d) throws SQLException{
-        String updateModul = "UPDATE Modules SET departmentCode = ? WHERE moduleCode = ?;";
-        try (PreparedStatement update = con.prepareStatement(updateModul)) {
-            update.clearParameters();
-            update.setString(1, d.getDeptCode());
-            update.setString(2, m.getModuleCode());
-            update.executeUpdate();
+    private void insertStudyPeriod(StudyPeriod newPeriod, Student student) throws SQLException {
+        
+        String insertPeriod = "INSERT INTO StudentsToModules VALUES(?,?,?,?,?,?,?);";
+        
+        try (PreparedStatement insert = con.prepareStatement(insertPeriod)) {
+            for (Grade g : newPeriod.getGradesList()) {
+                String[] names = {"regNum","moduleCode","label"};
+                String[] values = {Integer.toString(student.getRegNum()), g.getModule().getModuleCode(), ""+newPeriod.getLabel()}; 
+                if(!ValueSetCheck("StudentsToModules", names, values)){
+                    insert.clearParameters();
+                    insert.setInt(1, student.getRegNum());
+                    insert.setString(2, g.getModule().getModuleCode());
+                    insert.setDouble(3, g.getMark());
+                    insert.setDouble(4, g.getResitMark());
+                    insert.setString(5, "" + newPeriod.getLabel());
+                    insert.setString(6, student.getCourse().getCourseCode());
+                    insert.setString(7, "" + newPeriod.getDegreeLvl().getDegreeLvl());
+                    insert.executeUpdate();
+                }
+            }
         } catch (SQLException e) {
             throw e;
         }
     }
 
+    private void insertStudent(Student newStudent) throws SQLException {
+        
+        String[] names = { "regNum" };
+        String[] values = { Integer.toString(newStudent.getRegNum()) };
+        
+        if(!ValueSetCheck("Students", names, values)){
+            String insertStudent = "INSERT INTO Students VALUES(?,?,?,?,?,?,?,?)";
+            try (PreparedStatement insert = con.prepareStatement(insertStudent)) {
+                insert.clearParameters();
+                insert.setInt(1, newStudent.getRegNum());
+                insert.setString(2, newStudent.getTitle());
+                insert.setString(3, newStudent.getSurname());
+                insert.setString(4, newStudent.getForenames());
+                insert.setString(5, newStudent.getEmail());
+                insert.setString(6, newStudent.getUsername());
+                insert.setString(7, newStudent.getTutor());
+                insert.setString(8, newStudent.getCourse().getCourseCode());
+                insert.executeUpdate();
+                for (StudyPeriod x : newStudent.getStudyPeriodList()) {
+                    insertStudyPeriod(x, newStudent);
+                }
+            } catch (SQLException e) {
+                throw e;
+            }
+        }
+    }
+
+    private void insertTeacher(Teacher newTeacher) throws SQLException {
+        
+        String[] names = { "username" };
+        String[] values = { newTeacher.getUsername() };
+        
+        if (!ValueSetCheck("Students", names, values)) {
+            String insertTeacher = "INSERT INTO Teachers VALUES(?,?)";
+            try (PreparedStatement insert = con.prepareStatement(insertTeacher)) {
+                insert.clearParameters();
+                insert.setString(1, newTeacher.getUsername());
+                insert.setString(2, newTeacher.getFullName());
+                insert.executeUpdate();
+
+            } catch (SQLException e) {
+                throw e;
+            }
+        }
+    }
+
+    private void insertCourse(Course c) throws SQLException {
+
+        String[] names = { "courseCode" };
+        String[] values = { c.getCourseCode() };
+
+        if (!ValueSetCheck("Course", names, values)) {
+
+            String insertCourse = "INSERT INTO Course VALUES(?,?,?)";
+            try (PreparedStatement insert = con.prepareStatement(insertCourse)) {
+                insert.clearParameters();
+                insert.setString(1, c.getCourseCode());
+                insert.setString(2, c.getFullName());
+                insert.setBoolean(3, c.getYearInIndustry());
+                insert.executeUpdate();
+
+                if (c.getBachEquiv() != null) {
+                    insertBachEquiv(c, c.getBachEquiv());
+                }
+            } catch (Exception e) {
+                throw e;
+            }
+
+        }
+
+    }
+
+    private void insertCourseDepartLink(Course c, Department d) throws SQLException {
+        String insertLink = "INSERT INTO CourseToDepartment VALUES(?,?,?);";
+        try (PreparedStatement insert = con.prepareStatement(insertLink)) {
+            insert.clearParameters();
+            insert.setString(1, c.getCourseCode());
+            insert.setString(2, d.getDeptCode());
+            insert.setBoolean(3, c.getMainDep() == d);
+            insert.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        }
+    }
+
+    private void insertBachEquiv(Course master, Course equiv) throws SQLException {
+
+        String[] names = { "curseCode" };
+        String[] values = { master.getCourseCode() };
+        if (!ValueSetCheck("BachEquiv", names, values)) {
+            String insertEquiv = "INSERT INTO BachEqiv VALUES(?,?);";
+            try (PreparedStatement insert = con.prepareStatement(insertEquiv)) {
+                insert.clearParameters();
+                insert.setString(1, master.getCourseCode());
+                insert.setString(2, equiv.getCourseCode());
+                insert.executeUpdate();
+                
+            } catch (SQLException e) {
+                throw e;
+            }
+
+        }
+        
+    }
+
+    //UPDATES - PUBLIC FUNCTIONS
+
     /**
-     * Updates Supervidion of the module
+     * Updates Supervision of the module
      * @param m module in question
      * @param d new supervising department
      * @return true if operation was succesfull false otherwise
@@ -313,6 +409,20 @@ public class Database implements AutoCloseable {
             e.printStackTrace();
         }
         return succes;
+    }
+
+    //UPDATES - PRIVATE FUNCTIONS
+
+    private void updateModuleSupervision(Module m, Department d) throws SQLException {
+        String updateModul = "UPDATE Modules SET departmentCode = ? WHERE moduleCode = ?;";
+        try (PreparedStatement update = con.prepareStatement(updateModul)) {
+            update.clearParameters();
+            update.setString(1, d.getDeptCode());
+            update.setString(2, m.getModuleCode());
+            update.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        }
     }
 
 }
