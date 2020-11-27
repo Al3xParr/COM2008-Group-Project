c506feedcb5292077860325188b3b37b1f80dc72
Alex Parr <alexparr11@gmailcom>
2020-11-27 14:17:08 +0000

    Added ability to click on table

--- teamProject/src/teamProject/interface/AllAccounts.java
+++ teamProject/src/teamProject/interface/AllAccounts.java
@@ -8,7 +8,7 @@ public class AllAccounts extends JPanel{
     public AllAccounts(HashMap<String, Student> students, HashMap<String, Registrar> registrars, 
                         HashMap<String, Administrator> admins, HashMap<String, Teacher> teachers) {
         
-        String[] colNames = {"Username", "Access Level", " ", " "};
+        String[] colNames = {"Username", "Access Level", "", ""};
         int totalEntries = students.size() + registrars.size() + admins.size() + teachers.size();
         Object[][] accounts = new Object[4][totalEntries];
 
@@ -46,6 +46,48 @@ public class AllAccounts extends JPanel{
             counter ++;
         }
         JTable table = new JTable(accounts, colNames);
+
+        table.addMouseListener(new java.awt.event.MouseAdapter() {
+        @Override
+        public void mouseClicked(java.awt.event.MouseEvent evt) {
+            int row = jTable1.rowAtPoint(evt.getPoint());
+            int col = jTable1.columnAtPoint(evt.getPoint());
+            
+            if (col == 4){
+                String confirmStr = "Are you sure you want to delete " + accounts[4][row] + "?"
+                int dialogResult = JOptionPane.showConfirmDialog(null, confirmStr,"Warning", JOptionPane.YES_NO_OPTION);
+                if(dialogResult == JOptionPane.YES_OPTION){
+                    //Delete user accounts[4][row]
+                }
+                
+            }
+            else if (col == 3){
+                String[] possibleValues = { "Student", "Teacher", "Registrar", "Admin" };
+                String selectedValue = JOptionPane.showInputDialog(null,
+                    "Choose access level", "Access Level Change",
+                    JOptionPane.INFORMATION_MESSAGE, null,
+                    possibleValues, possibleValues[0]);
+                
+                int levelNum;
+                switch(selectedValue){
+                    case "Student":
+                        levelNum = 1;
+                        break;
+                    case "Teacher":
+                        levelNum = 2;
+                        break;
+                    case "Registrar":
+                        levelNum = 3;
+                        break;
+                    case "Admin":
+                        levelNum = 4;
+                        break;
+                }
+                //changePriv of user accounts[4][row] to levelNum
+            }
+
+        }
+    });
     }
 
 }
